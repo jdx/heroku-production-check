feb3d6d71d95c16d781f3c40d2497138ed84addf
dickeyxxx <jeff@dickeyxxx.com>
2014-12-02 23:32:09 -0800

    new interface

--- test.js
+++ test.js
@@ -1,9 +1,7 @@
-var netrc = require('netrc')()
-var plugin = require('./index')
+var netrc = require('netrc')();
+var plugin = require('./index');
 
-plugin.topics[0].commands[0].run({
+plugin.commands[0].run({
   app: 'dickey-xxx',
-  auth: {
-    password: netrc['api.heroku.com']['password']
-  }
-})
+  auth: { password: netrc['api.heroku.com'].password }
+});
